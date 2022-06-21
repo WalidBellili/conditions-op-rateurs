7328ad07d8a3bc86ffd8382122157631f6f6516f
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-21 14:54:10 +0200

    exo 7

--- index.js
+++ index.js
@@ -59,45 +59,69 @@
 // }
 
 // exo6
-const month = "January"
+// const month = "January"
 
-switch (month) {
-    case  "January":
-        console.log("winter");
-    break;
-    case "February":
-        console.log("winter");
-    break;
-    case "Mach":
-        console.log("winter");
-    break;
-    case 'April':
-        console.log("winter");
-        break;
-    case 'May':
-        console.log("winter");
-        break;
-    case 'June':
-        console.log("Summer");
-        break;
-    case 'Jully':
-        console.log("Summer");
-        break;
-    case 'August':
-        console.log("Summer");
-        break;
-    case 'Septembre':
-        console.log("Summer");
-        break;
-    case 'Octobre':
-        console.log("Fall");
-        break;
-    case 'Novembre':
-        console.log("Fall");
-        break;
-    case 'Décembre':
-        console.log("winter");
-        break;
-        default:
-            console.log("ok");
+// switch (month) {
+//     case  "January":
+//         console.log("winter");
+//     break;
+//     case "February":
+//         console.log("winter");
+//     break;
+//     case "Mach":
+//         console.log("winter");
+//     break;
+//     case 'April':
+//         console.log("winter");
+//         break;
+//     case 'May':
+//         console.log("winter");
+//         break;
+//     case 'June':
+//         console.log("Summer");
+//         break;
+//     case 'Jully':
+//         console.log("Summer");
+//         break;
+//     case 'August':
+//         console.log("Summer");
+//         break;
+//     case 'Septembre':
+//         console.log("Summer");
+//         break;
+//     case 'Octobre':
+//         console.log("Fall");
+//         break;
+//     case 'Novembre':
+//         console.log("Fall");
+//         break;
+//     case 'Décembre':
+//         console.log("winter");
+//         break;
+//         default:
+//             console.log("ok");
+// }
+
+// exo7
+
+// const roundedNumber = 12.4
+// const round =    roundedNumber - Math.floor(roundedNumber)
+// if (round >= 0.5) {
+//     console.log(Math.ceil(roundedNumber));
+// } else {
+//     console.log(Math.floor(roundedNumber));
+// }
+
+
+const roundedNumber = 16662.47777
+const round =    roundedNumber.toString() 
+const ind = round.indexOf(".")+1 
+const sub = round.substring(ind, ind +1)
+
+
+
+if (parseInt(round) >= 0.5) {
+    console.log(Math.ceil(roundedNumber));
+} else {
+    console.log(Math.floor(roundedNumber));
 }
